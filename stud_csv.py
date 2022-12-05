ac95c57332e8e982395bb669d2a1d4db3928196e
GudkovaO <119728125+GudkovaO@users.noreply.github.com>
2022-12-05 11:31:52 +0300

    Создали  добавление данных

--- stud_csv.py
+++ stud_csv.py
@@ -10,3 +10,19 @@ def file_open(file_name):
         for row in reader:
             csv_file.append(row)
         print("Файл открыт. Записей: ", len(csv_file))
+
+# Добавление
+
+def insert(bilet, fio, gender, age, tel, email, group, course):
+    try:
+        csv_file.append({'номер билета': bilet,
+                         'фио': fio,
+                         'пол': gender,
+                         'возраст': age,
+                         'телефон': tel,
+                         'почта': email,
+                         'группа': group,
+                         'курс': course})
+        except Exception as e:
+            return f"Ошибка при добавлении записи {e}"
+        print("Данные добавлены")
