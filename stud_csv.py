f06beb03949f8080878218391d9827412c2417fd
GudkovaO <119728125+GudkovaO@users.noreply.github.com>
2022-12-05 15:39:47 +0300

    Создали  Поиск по учебной группе

--- stud_csv.py
+++ stud_csv.py
@@ -39,3 +39,8 @@ def drop_by_arg(val, col_name="номер билета"):
 # Поиск по ФИО
 def find(val, col_name="фио"):
     print(*list(filter(lambda x: x[col_name] == val, csv_file)))
+
+
+# Поиск по группе
+def find_group(val, col_name="группа"):
+    print(*list(filter(lambda x: x[col_name] == val, csv_file)))
