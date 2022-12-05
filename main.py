18e26717c155af65307d1f99f0d44e70eefda4b3
GudkovaO <119728125+GudkovaO@users.noreply.github.com>
2022-12-05 15:37:39 +0300

    Создали  Удаление данных

--- main.py
+++ main.py
@@ -29,3 +29,18 @@ while True:
  elif action == '2':
         insert(input('Номер билета: '), input('ФИО: '), input('Пол: '), input('Возраст: '), input('Телефон: '),
                input('Почта: '), input('Группа: '), input('Курс: '))
+elif action == '3':
+print(drop_by_arg(input("Значение: "), input("Колонка: ")))
+elif action == '4':
+find(input("Значение: "), input("Колонка: "))
+elif action == '5':
+find_group(input("Значение: "), input("Колонка: "))
+elif action == '9':
+save()
+elif action == '10':
+show_rows()
+elif action == '0':
+for k, v in MENU.items():
+    print(k, '-', v)
+elif action == 'exit':
+break
